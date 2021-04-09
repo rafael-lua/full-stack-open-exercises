5a8b464c80998fe9c5281c6530328e8a6a3bedab
Rafael <rafael_oliveira@gmx.com>
2021-04-09 16:54:36 -0300

    Exercise part1a - 1.1

--- part1/1a_course-information/src/App.js
+++ part1/1a_course-information/src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      {props.total}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +33,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total />
     </div>
   )
 }
