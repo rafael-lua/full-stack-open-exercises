5bcb74cff16441e01898d004802234a7c6a4fa37
Rafael <rafael_oliveira@gmx.com>
2021-04-15 15:06:03 -0300

    Exercise part2b-2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,27 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '1-123-1234567' }
   ]); 
   const [ newName, setNewName ] = useState('');
+  const [ newPhone, setNewPhone ] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
 
-  const addName = (e) => {
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
+  };
+
+  const addPerson = (e) => {
     e.preventDefault();
     if(persons.filter((value) => value.name === newName).length === 0) {
-      let newNameObject = {
-        name: newName
+      let newPerson = {
+        name: newName,
+        phone: newPhone
       };
-      setPersons([...persons, newNameObject]);
+      setPersons([...persons, newPerson]);
     } else {
       window.alert(`${newName} is already on the list!`);
     }
@@ -30,12 +36,15 @@ const App = () => {
           New Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={addName}>Add</button>
+          Number: <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((value) => <li key={value.name}>{value.name}</li>)}
+        {persons.map((value) => <li key={value.name}>{value.name} {value.phone}</li>)}
       </ul>
     </div>
   )
