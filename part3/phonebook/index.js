db90f990bb8abee287b56e28bb64d2ab91398a1a
Rafael <rafael_oliveira@gmx.com>
2021-04-23 18:01:03 -0300

    Exercise part3a-3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 app.use(express.json());
+
+morgan.token("post-data", function (req, res) { 
+  return req.method === "POST" ? JSON.stringify(req.body) : null;
+});
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post-data"));
 
 let persons = [
   {
