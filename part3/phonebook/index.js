ff62520c519a0c265f87949ce4517f43a676506d
Rafael <rafael_oliveira@gmx.com>
2021-04-26 16:33:28 -0300

    Add cors package

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,8 @@ morgan.token("post-data", function (req, res) {
 });
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post-data"));
+
+app.use(cors());
 
 let persons = [
   {
