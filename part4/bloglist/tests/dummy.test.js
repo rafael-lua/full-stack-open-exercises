8b869d9e12f784dfda65dc121cccd7d424d2f433
Rafael <rafael_oliveira@gmx.com>
2021-05-01 17:55:40 -0300

    Exercise part4a-4.4

---
+++ part4/bloglist/tests/dummy.test.js
@@ -0,0 +1,8 @@
+const list_helper = require("../utils/list_helper")
+
+test("dummy returns one", () => {
+  const blogs = []
+
+  const result = list_helper.dummy(blogs)
+  expect(result).toBe(1)
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    return blogs.reduce((total, blog) => {
+      total += blog.likes
+      return total
+    }, 0)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
