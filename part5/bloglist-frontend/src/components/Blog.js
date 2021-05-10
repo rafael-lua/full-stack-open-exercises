85adb50186cab53fe629ae0acc5999299fdf4f94
Rafael <rafael_oliveira@gmx.com>
2021-05-10 10:41:46 -0300

    Exercise part5a-5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const onShowState = { display: visible ? "" : "none" }
+
+  return (
+    <div className="blog-block">
+      <p><strong>{blog.title}</strong> <button onClick={() => {setVisible(!visible)}}>{ visible ? "Hide" : "Show"}</button></p>
+      <p style={onShowState}>{blog.url}</p>
+      <p style={onShowState}>{blog.likes} <button>Like</button></p>
+      <p style={onShowState}>{blog.author}</p>
+    </div>
+  )
+}
 
 export default Blog
