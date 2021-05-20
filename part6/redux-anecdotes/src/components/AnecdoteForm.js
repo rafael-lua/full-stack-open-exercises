831b4b3236d6243338e5e264ce59cbb2da16b118
Rafael <rafael_oliveira@gmx.com>
2021-05-20 16:35:40 -0300

    Exercise part6d-6.20

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,22 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+// import { useDispatch } from "react-redux"
+import { connect } from "react-redux"
 import { createAnecdote, sortAnecdotes } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
 
 // import anecdotesService from "../services/anecdotes"
 
-const AnecdoteForm = () => {
-  const dispatch = useDispatch()
+const AnecdoteForm = (props) => {
+  // const dispatch = useDispatch()
 
   const create = async (e) => {
     e.preventDefault()
 
     // const newAnec = await anecdotesService.create(e.target.anecdote.value)
-    dispatch(createAnecdote(e.target.anecdote.value))
-    dispatch(sortAnecdotes())
+    props.createAnecdote(e.target.anecdote.value)
+    props.sortAnecdotes()
     const notifMsg = `Created the anecdote: ${e.target.anecdote.value}`
-    dispatch(setNotification(notifMsg, 5))
+    props.setNotification(notifMsg, 5)
     // setTimeout(() => dispatch(removeNotification(notifMsg)), 5000)
   }
 
@@ -30,4 +31,12 @@ const AnecdoteForm = () => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  setNotification,
+  createAnecdote,
+  sortAnecdotes
+}
+
+const ConnectedAnecdoteForm = connect(null, mapDispatchToProps)(AnecdoteForm)
+
+export default ConnectedAnecdoteForm
--- part6/redux-anecdotes/src/components/FilterAnecdotes.js
+++ part6/redux-anecdotes/src/components/FilterAnecdotes.js
@@ -1,14 +1,15 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+// import { useDispatch } from "react-redux"
+import { connect } from "react-redux"
 import { setFilter } from "../reducers/filterReducer"
 
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
+  // const dispatch = useDispatch()
 
   const handleChange = (event) => {
     // input-field value is in variable event.target.value
-    dispatch(setFilter(event.target.value))
+    props.setFilter(event.target.value)
   }
   const style = {
     marginBottom: 10
@@ -21,4 +22,10 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = {
+  setFilter
+}
+
+const ConnectedFilter = connect(null, mapDispatchToProps)(Filter)
+
+export default ConnectedFilter
