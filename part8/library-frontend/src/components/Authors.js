65a6072fbc8fe64527098e60f4afc039e8babc41
Rafael <rafael_oliveira@gmx.com>
2021-05-31 10:52:14 -0300

    Exercise part8b-8.8

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useQuery } from "@apollo/client"
+
+import { ALL_AUTHORS } from "../queries"
+
+const Authors = ({ show }) => {
+  const authors = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000
+  })
+
+  if (!show) {
+    return null
+  } else if (authors.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
