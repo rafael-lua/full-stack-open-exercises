a8f1c6826fd01db60ab98201b2ce12e59399c194
Rafael <rafael_oliveira@gmx.com>
2021-06-04 09:08:51 -0300

    Exercise part8d-8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useQuery } from "@apollo/client"
 
 import { ALL_BOOKS } from "../queries"
 
-const Books = ({show}) => {
+const Books = ({ show }) => {
+  const [genres, setGenres] = useState([])
+  const [filterGenre, setFilterGenre] = useState(null)
+
   const books = useQuery(ALL_BOOKS, {
     pollInterval: 2000
   })
+
+  const setGenresCallback = useCallback((g) => {
+    setGenres(g)
+  }, [])
+
+  useEffect(() => {
+    if (books.data) {
+      let genresList = []
+      const checkGenreAndInsert = (book) => {
+        book.genres.forEach((genre) => {
+          if (!genresList.includes(genre)) {
+            genresList = genresList.concat(genre)
+          }
+        })
+      }
+      books.data.allBooks.forEach(checkGenreAndInsert)
+      setGenresCallback(genresList)
+    }
+  }, [books.data, setGenresCallback])
+
+  const filterBy = (g) => {
+    setFilterGenre(g)
+  }
+
+  const filteredBooks = () => {
+    let boosPerGenre = [...books.data.allBooks]
+
+    if (filterGenre) {
+      boosPerGenre = boosPerGenre.filter((book) => book.genres.includes(filterGenre))
+    }
+
+    return (
+      boosPerGenre.map(a =>
+        <tr key={a.title}>
+          <td>{a.title}</td>
+          <td>{a.author.name}</td>
+          <td>{a.published}</td>
+        </tr>
+      )
+    )
+  }
 
   if (!show) {
     return null
@@ -14,11 +58,10 @@ const Books = ({show}) => {
     return <div>loading...</div>
   }
 
-
   return (
     <div>
       <h2>books</h2>
-
+      <p>in genre: <strong>{ filterGenre ? filterGenre : "all"}</strong></p>
       <table>
         <tbody>
           <tr>
@@ -30,15 +73,17 @@ const Books = ({show}) => {
               published
             </th>
           </tr>
-          {books.data.allBooks.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
+          {filteredBooks()}
         </tbody>
       </table>
+      <button onClick={() => {filterBy(null)}}>all genres</button>
+      {
+        genres
+          ? genres.map((genre) => {
+            return <button key={genre} onClick={() => {filterBy(genre)}}>{ genre }</button>
+          })
+          : null
+      }
     </div>
   )
 }
